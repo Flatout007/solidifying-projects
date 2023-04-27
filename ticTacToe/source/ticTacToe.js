bb592b997fddfbb98a3cd0f47d336568e1b07221
Flatout007 <thechamp301@gmail.com>
2023-04-27 12:23:08 -0400

    added new features and styling

--- ticTacToe/source/ticTacToe.js
+++ ticTacToe/source/ticTacToe.js
@@ -1,264 +1,190 @@
-class TicTacToe {
-
-    public newGameButton: HTMLElement;
-    public board: Array<Array<any>>;
-    public currentPlayer: string;
-    public element: HTMLDivElement;
-    public isGameEnded: boolean;
-
-    constructor(board: Array<Array<any>>, currentPlayer: string, element: HTMLDivElement, newGameButton: HTMLElement, isGameEnded: boolean) {
+var TicTacToe = /** @class */ (function () {
+    function TicTacToe(board, currentPlayer, element, newGameButton, isGameEnded) {
+        var _this = this;
         this.board = board;
         this.currentPlayer = currentPlayer;
         this.element = element;
         this.isGameEnded = isGameEnded;
         this.newGameButton = newGameButton;
-
         // events
-        this.element.addEventListener("click", (e: Event): void => {
-            this.handlePlayerClick(e);
+        this.element.addEventListener("click", function (e) {
+            _this.handlePlayerClick(e);
         });
     }
-
-    switchPlayer(): void {
-
+    TicTacToe.prototype.switchPlayer = function () {
         if (this.currentPlayer === "X")
             this.currentPlayer = "O";
         else
             this.currentPlayer = "X";
-
-    }
-
-    isWinRow(): boolean {
-
-        for (let i = 0; i < this.board.length; i++) {
-
-            const won = this.board[i].every((ele: string): boolean => {
-                return ele === this.currentPlayer;
+    };
+    TicTacToe.prototype.isWinRow = function () {
+        var _this = this;
+        for (var i = 0; i < this.board.length; i++) {
+            var won = this.board[i].every(function (ele) {
+                return ele === _this.currentPlayer;
             });
-
             if (won)
                 return true;
         }
-
         return false;
-    }
-
-    isWinColumn(): boolean {
-
-        for (let i = 0; i < this.board.length; i++) {
-
-            const array: Array<string> = [];
-
-            for (let j = 0; j < this.board.length; j++) {
+    };
+    TicTacToe.prototype.isWinColumn = function () {
+        var _this = this;
+        for (var i = 0; i < this.board.length; i++) {
+            var array = [];
+            for (var j = 0; j < this.board.length; j++) {
                 array.push(this.board[j][i]);
             }
-
-            const won = array.every((ele: string): boolean => {
-                return ele === this.currentPlayer;
+            var won = array.every(function (ele) {
+                return ele === _this.currentPlayer;
             });
-
             if (won)
                 return true;
         }
-
         return false;
-    }
-
-    isWinDiagonal(): boolean {
-        const main: Array<string> = [];
-        const secondary: Array<string> = [];
-        let count = 1;
-
-        for (let i = 0; i < this.board.length; i++) {
-
-            for (let j = 0; j < this.board.length; j++) {
-
+    };
+    TicTacToe.prototype.isWinDiagonal = function () {
+        var _this = this;
+        var main = [];
+        var secondary = [];
+        var count = 1;
+        for (var i = 0; i < this.board.length; i++) {
+            for (var j = 0; j < this.board.length; j++) {
                 if (i === j) {
                     main.push(this.board[i][j]);
                 }
             }
-
             secondary.push(this.board[i][this.board[i].length - count]);
-
             // increment count to subtract from row length and get next element of secondary diagonal
             count++;
         }
-
-        const wonMain = main.every((ele: string): boolean => {
-            return ele === this.currentPlayer;
+        var wonMain = main.every(function (ele) {
+            return ele === _this.currentPlayer;
         });
-
-        const wonSecondary = secondary.every((ele: string): boolean => {
-            return ele === this.currentPlayer;
+        var wonSecondary = secondary.every(function (ele) {
+            return ele === _this.currentPlayer;
         });
-
         if (wonMain || wonSecondary) {
             return true;
         }
-
         return false;
-    }
-
+    };
     // place the current players mark on click, and call the render method,
     // and switches the player
-    handlePlayerClick(e: Event): void {
-        const element = e.target as HTMLElement;
-        const row = parseFloat(element.getAttribute("row") as string);
-        const col = parseFloat(element.getAttribute("col") as string);
-        const coordinates = [row, col];
-
+    TicTacToe.prototype.handlePlayerClick = function (e) {
+        var element = e.target;
+        var row = parseFloat(element.getAttribute("row"));
+        var col = parseFloat(element.getAttribute("col"));
+        var coordinates = [row, col];
         // if the player clicks on a cell after the game has ended,
         // clear the board before clicking, and start a new game
         if (this.isGameEnded) {
             this.isGameEnded = false;
         }
-
         if (this.isValid(coordinates)) {
-            const [x, y] = coordinates;
-
+            var x = coordinates[0], y = coordinates[1];
             if (this.board[row][col] === "") {
-
                 if (this.currentPlayer === "X") {
-
                     this.board[x][y] = "X";
-                } else {
+                }
+                else {
                     this.board[x][y] = "O";
                 }
-            } else {
+            }
+            else {
                 alert("space already taken");
                 return;
             }
         }
-
         // re-render the game board to update the HTML
         this.render(this.board);
-
         if (this.isWinColumn() || this.isWinDiagonal() || this.isWinRow()) {
-            alert(`player ${this.currentPlayer} has won the game`);
+            alert("player ".concat(this.currentPlayer, " has won the game"));
             this.startOver();
         }
-
         this.switchPlayer();
-
         console.log(this);
-    }
-
-    bindEvent(event: string, element: HTMLElement): void {
-        element.addEventListener(event, () => {
+    };
+    TicTacToe.prototype.bindEvent = function (event, element) {
+        element.addEventListener(event, function () {
             location.reload();
         });
-    }
-
+    };
     // checks if the row, col indices are in bounds or not
-    isValid(coordinates: Array<number>): boolean {
-        const [x, y] = coordinates;
-
+    TicTacToe.prototype.isValid = function (coordinates) {
+        var x = coordinates[0], y = coordinates[1];
         if (x < 0 || y < 0) {
             return false;
         }
-
         if (y > this.board.length - 1 || x > this.board.length - 1) {
             return false;
         }
-
         return true;
-    }
-
-
+    };
     // renders the board as valid HTML
-    render(board: Array<Array<any>>): void {
-
+    TicTacToe.prototype.render = function (board) {
         this.bindEvent("click", this.newGameButton);
-
         this.element.innerHTML = "";
-
         // loop through the current instance of board 
-        for (let i = 0; i < board.length; i++) {
-
-            for (let j = 0; j < board.length; j++) {
-
-                const p = document.createElement("p");
-                const cell = document.createElement("li");
-
+        for (var i = 0; i < board.length; i++) {
+            for (var j = 0; j < board.length; j++) {
+                var p = document.createElement("p");
+                var cell = document.createElement("li");
                 cell.setAttribute("class", "cell");
                 cell.setAttribute("row", i.toString());
                 cell.setAttribute("col", j.toString());
                 p.innerHTML = this.board[i][j];
                 cell.appendChild(p);
-
                 // append element to entry point
                 this.element.append(cell);
             }
         }
-    }
-
-    startOver(): void {
-
+    };
+    TicTacToe.prototype.startOver = function () {
         this.isGameEnded = true;
-
         this.board = TicTacToe.newGame(this.board.length);
-
         this.appendGameBoard();
-
         this.removeEvent("click", this.startOver);
-    }
-
-    removeEvent(event: string, method: (param?: any) => void): void {
+    };
+    TicTacToe.prototype.removeEvent = function (event, method) {
         this.newGameButton.removeEventListener(event, method);
-    }
-
-    appendGameBoard(): void {
-        const newGameBoard = document.createElement("div");
-
+    };
+    TicTacToe.prototype.appendGameBoard = function () {
+        var _a;
+        var newGameBoard = document.createElement("div");
         newGameBoard.classList.add("game-container");
-
         // create tiles
-        for (let i = 0; i < this.board.length; i++) {
-
-            for (let j = 0; j < this.board.length; j++) {
-
-                const p = document.createElement("p");
-                const cell = document.createElement("li");
-
+        for (var i = 0; i < this.board.length; i++) {
+            for (var j = 0; j < this.board.length; j++) {
+                var p = document.createElement("p");
+                var cell = document.createElement("li");
                 cell.setAttribute("class", "cell");
                 cell.setAttribute("row", i.toString());
                 cell.setAttribute("col", j.toString());
                 p.innerHTML = this.board[i][j];
                 cell.appendChild(p);
-
                 newGameBoard.append(cell);
             }
         }
-
-        this.element.parentNode?.insertBefore(newGameBoard, this.element.nextSibling);
-
+        (_a = this.element.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(newGameBoard, this.element.nextSibling);
         // start new instance of tic-tac-toe
         new TicTacToe(TicTacToe.newGame(this.board.length), "X", newGameBoard, this.newGameButton, false);
-    }
-
-
-    static newGame(n: number): Array<Array<any>> {
-        const array: Array<Array<any>> = [];
-
-        for (let i = 0; i < n; i++) {
-
+    };
+    TicTacToe.newGame = function (n) {
+        var array = [];
+        for (var i = 0; i < n; i++) {
             array.push(new Array());
-
-            for (let j = 0; j < n; j++) {
+            for (var j = 0; j < n; j++) {
                 array[i].push("");
             }
         }
-
         return array;
-    }
-}
-
-function main(element: HTMLDivElement, newGameButton: HTMLElement, n: number): void {
-    const board = TicTacToe.newGame(n);
-
-    const ticTacToe = new TicTacToe(board, "X", element, newGameButton, false);
-
+    };
+    return TicTacToe;
+}());
+function main(element, newGameButton, n) {
+    var board = TicTacToe.newGame(n);
+    var ticTacToe = new TicTacToe(board, "X", element, newGameButton, false);
     console.log(ticTacToe);
-
     ticTacToe.render(board);
 }
